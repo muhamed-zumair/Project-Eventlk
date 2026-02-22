27e82dc5b24524756e2830ed8472be26b539a62f
Rism583 <risme1522@gmail.com>
2026-02-22 21:59:00 +0530

    contact page form added in main server file

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app=express();
 app.use(express.json());
 
 const authRoutes = require('../src/routes/authRoutes');
+const contactRoutes = require('../src/routes/contactRoutes');
 
 app.use('/api/auth' , authRoutes);
+app.use('/api', contactRoutes);
 
 const PORT = 5000;
 
