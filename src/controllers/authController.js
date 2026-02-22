d08c24249ed54424e42cde23fa75bf5b1f4d33ae
Rism583 <risme1522@gmail.com>
2026-02-22 21:36:36 +0530

    sign in controller added with all validations

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,4 @@
+//function to handle the user registration
 const registerUser = async(req, res) => {
 
     // Extract user details from the request body
@@ -44,8 +45,42 @@ const registerUser = async(req, res) => {
     })
 }
 
+const loginUser = async(req, res) => {
+
+    const {email, password} = req.body;
+
+    //1. Validation check for empty fields
+    if (!email || !password){
+        return res.status(400).json({
+            success: false,
+            message: "Please fill the required fields!"
+        })
+    }
+    //2. Validation check for email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)){   
+        return res.status(400).json({
+            success: false,
+            message: "Please provide a valid email address!"
+        })
+    }
+
+    //3. Check if the user exists in database
+
+    //4. Compare the provided password with the hashed password in the database using bcrypt
+
+    //5. If the password matches, generate a JWT token for authentication
+
+    //temporary response until database integration is done
+    return res.status(200).json({
+        success:true,
+        message: "User Logged in Successfully!"
+    })
+}
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
 
 
