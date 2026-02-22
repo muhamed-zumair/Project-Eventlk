894f78598ba46bf948d6d967d1e82fd6fa735286
Rism583 <risme1522@gmail.com>
2026-02-22 21:38:15 +0530

    setted up the auth router with post for signin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser} = require('./controllers/authController')
+const {registerUser, loginUser} = require('./controllers/authController')
 
 router.post('/signup',  registerUser);
+router.post('/signin',  loginUser);
 
 module.exports = router;
