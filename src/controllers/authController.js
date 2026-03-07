9d591e3266c85ee647a8c39cf6ef0cdfa80ef912
Rism583 <risme1522@gmail.com>
2026-03-07 23:35:00 +0530

    Databse Integration for signup page done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,7 @@
+const {v4: uuidv4} = require('uuid');
+const pool=require ('../config/db');
+const bycrypt = require('bcryptjs');
+
 //function to handle the user registration
 const registerUser = async(req, res) => {
 
@@ -31,18 +35,47 @@ const registerUser = async(req, res) => {
             message: "Password must be atleast 8 characters long and contain at least one letter, one number and one special character!"
         })
     }
+    try{
+        //4. Check if the user already exists in database
+        const checkUserQuery = 'SELECT * FROM "Users" WHERE email = $1';
+        const existingUser = await pool.query(checkUserQuery, [email]);
 
-    //4. Check if the user already exists in database
+        if (existingUser.rows.length > 0){
+            return res.status(409).json({
+                success: false,
+                message: "User with this email already exists!"
+            })
+        }
 
-    //5.Hash the password using bcrypt
+        //5.Hash the password using bcrypt
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(password, salt);
 
-    //saving the new user to the database
+        //saving the new user to the database
+        const newUserId = uuidv4(); // Generate a unique ID for the user
+        const defaultRole = 'Attendee';
 
-    //temporary response until databser integration is done
-    return res.status(201).json({
-        success:true,
-        message: "User Registered Successfully!"
-    })
+        const orgId = organization ? organization : null; // Generate an organization ID if organization is provided
+
+        const insertUserQuery = `INSERT INTO "Users" (id, first_name, last_name, email, password_hash, role, organization_id) 
+                                 VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+        const values = [newUserId, firstName, lastName, email, hashedPassword, defaultRole, orgId];
+        await pool.query(insertUserQuery, values);
+
+        return res.status(201).json({
+            success:true,
+            message: "User Registered Successfully!"
+        })
+    } catch (error) {
+        console.error("Error occurred while registering user:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error; Could not register user at this time."
+        });
+
+
+    }
+    
 }
 
 const loginUser = async(req, res) => {
